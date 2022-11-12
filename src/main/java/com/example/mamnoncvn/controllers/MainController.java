a5bc9042f6d3da5a6b13353bb96c90b40fa642fa
Bui Phuoc <buiphuoc@DESKTOP-R6Q69GE.fsoft.fpt.vn>
2022-11-12 20:36:41 +0700

    update controller

--- src/main/java/com/example/mamnoncvn/controllers/MainController.java
+++ src/main/java/com/example/mamnoncvn/controllers/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String hello() {
-        return "hello";
+        return "hello phuoc dep trai";
     }
 }
