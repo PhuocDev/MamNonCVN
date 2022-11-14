859e1b576f494ac0485c44783a12d3f2459ac8da
Bui Phuoc <buiphuoc@Phuocs-Mac.local>
2022-11-15 00:25:09 +0700

    Hoan thanh thymeleaf template

--- src/main/java/com/example/mamnoncvn/controllers/MainController.java
+++ src/main/java/com/example/mamnoncvn/controllers/MainController.java
@@ -1,7 +1,15 @@
 package com.example.mamnoncvn.controllers;
+import com.example.mamnoncvn.GiaoVienCVN.Models.request.CreateGiaoVienRequest;
+import com.example.mamnoncvn.GiaoVienCVN.entity.GiaoVien;
+import com.example.mamnoncvn.GiaoVienCVN.service.GiaoVienService;
+import org.dom4j.rule.Mode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -16,5 +24,29 @@ public class MainController {
     @GetMapping("/student")
     public String student() {
         return "admin/student";
+    }
+
+
+    @Autowired
+    GiaoVienService giaoVienService;
+
+    @GetMapping("/teacher")
+    public String teacherManagement(Model model){
+        List<GiaoVien> giaoVienList = giaoVienService.getAllGV();
+        model.addAttribute("giaoVienList", giaoVienList);
+        return "admin/giaovien";
+    }
+    @GetMapping("/addGV")
+    public String addNewGV(@RequestParam String hoten, @RequestParam String sdt,@RequestParam String address,
+                           @RequestParam String email, @RequestParam Boolean trangThai)
+    {
+        CreateGiaoVienRequest giaoVienRequest = new CreateGiaoVienRequest(hoten, sdt, address,email,trangThai);
+        giaoVienService.addNewGiaoVien(giaoVienRequest);
+        return "redirect:/teacher";
+    }
+    @GetMapping("/deleteGV")
+    public String deleteGV(@RequestParam Long id) {
+        giaoVienService.deleteGiaoVienById(id);
+        return "redirect:/teacher";
     }
 }
