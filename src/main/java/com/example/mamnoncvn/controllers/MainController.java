c44ef39cdb7920c9ecf045f48e809eaeb68b5f7a
Phuoc Bui <phuocbui@kms.com.vn>
2022-11-15 14:23:57 +0700

    update controler in giaovien

--- src/main/java/com/example/mamnoncvn/controllers/MainController.java
+++ src/main/java/com/example/mamnoncvn/controllers/MainController.java
@@ -27,46 +27,4 @@ public class MainController {
         return "admin/student";
     }
 
-
-    @Autowired
-    GiaoVienService giaoVienService;
-
-    @GetMapping("/teacher")
-    public String teacherManagement(Model model){
-        List<GiaoVien> giaoVienList = giaoVienService.getAllGV();
-        model.addAttribute("giaoVienList", giaoVienList);
-        CreateGiaoVienRequest giaoVienRequest = new CreateGiaoVienRequest();
-        model.addAttribute("giaovienRequest", giaoVienRequest);
-        UpdateGiaoVienRequest updateGiaoVienRequest = new UpdateGiaoVienRequest();
-        model.addAttribute("updateGiaovienRequest", updateGiaoVienRequest);
-        return "admin/giaovien";
-    }
-    @GetMapping("/addGV")
-    public String addNewGV(@RequestParam String hoten, @RequestParam String sdt,@RequestParam String address,
-                           @RequestParam String email, @RequestParam Boolean trangThai)
-    {
-        CreateGiaoVienRequest giaoVienRequest = new CreateGiaoVienRequest(hoten, address, sdt,email,trangThai);
-        giaoVienService.addNewGiaoVien(giaoVienRequest);
-        return "redirect:/teacher";
-    }
-    //lưu ý phải sử dụng @ModelAttribute để phân biệt các request body
-    @PostMapping(path = "/addGV", consumes = "application/x-www-form-urlencoded")
-    public String addNewGVPost(@Valid @ModelAttribute("giaovienRequest") CreateGiaoVienRequest giaoVienRequest)
-    {
-        //CreateGiaoVienRequest giaoVienRequest = new CreateGiaoVienRequest(hoten, address, sdt,email,trangThai);
-        giaoVienService.addNewGiaoVien(giaoVienRequest);
-        return "redirect:/teacher";
-    }
-    @PostMapping(path = "/updateGV", consumes = "application/x-www-form-urlencoded")
-    public String updateGV(@Valid @ModelAttribute("updateGiaovienRequest") UpdateGiaoVienRequest updateGiaoVienRequest)
-    {
-        //CreateGiaoVienRequest giaoVienRequest = new CreateGiaoVienRequest(hoten, address, sdt,email,trangThai);
-        giaoVienService.updateGiaoVien(updateGiaoVienRequest);
-        return "redirect:/teacher";
-    }
-    @GetMapping("/deleteGV")
-    public String deleteGV(@RequestParam Long id) {
-        giaoVienService.deleteGiaoVienById(id);
-        return "redirect:/teacher";
-    }
 }
--- src/main/java/com/example/mamnoncvn/exception/ExceptionHandlingController.java
+++ src/main/java/com/example/mamnoncvn/exception/ExceptionHandlingController.java
@@ -1,8 +1,10 @@
 package com.example.mamnoncvn.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +12,9 @@ import javax.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class ExceptionHandlingController {
 
-
-
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String userNotFoundException(Exception e) {
+        return "error";
+    }
 }
