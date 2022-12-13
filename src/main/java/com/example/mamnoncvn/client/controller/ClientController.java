b67262f0fa278ddcf65b277122b41635e04b1956
Phuoc Bui <phuocbui@kms.com.vn>
2022-12-13 15:09:34 +0700

    complete view cth detail

--- src/main/java/com/example/mamnoncvn/client/controller/ClientController.java
+++ src/main/java/com/example/mamnoncvn/client/controller/ClientController.java
@@ -4,6 +4,7 @@ import com.example.mamnoncvn.chuongTrinhHoc.entity.ChuongTrinhHoc;
 import com.example.mamnoncvn.chuongTrinhHoc.service.ChuongTrinhHocService;
 import com.example.mamnoncvn.customer.models.request.CreateCustomerRequest;
 import com.example.mamnoncvn.customer.service.CustomerService;
+import com.example.mamnoncvn.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @Controller
@@ -55,6 +57,13 @@ public class ClientController {
     @GetMapping("/blogDetail")
     public String blogDetail(Model model) {
         return "client/blogDetail";
+    }
+    @GetMapping("/viewCthDetail")
+    public String viewCthDetail(Model model, @PathParam("id") Long id) {
+        ChuongTrinhHoc chuongTrinhHoc = chuongTrinhHocService.findChuongTrinhHocByID(id);
+        if (chuongTrinhHoc == null)  throw new NotFoundException("Cannot found CTH id: " + id);
+        model.addAttribute("chuongtrinhhoc", chuongTrinhHoc);
+        return "client/cthDetail";
     }
     @GetMapping("contact")
     public String contact(){
